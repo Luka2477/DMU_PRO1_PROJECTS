465c457f9f9e3b521be3be2bd1b74efe485e8ed8
Luka2477 <luke.da.duke@hotmail.com>
2021-10-11 15:07:13 +0200

    Init Project2_Fravaerssystem

---
+++ Project2_Fravaerssystem/src/afleveringfravaer/FravaersSystem.java
@@ -0,0 +1,75 @@
+package afleveringfravaer;
+
+public class FravaersSystem {
+	/**
+	 * Fraværstallene udskrives på skærmen
+	 *
+	 * @param fravaer
+	 */
+	public void udskrivFravaer(int[][] fravaer) {
+		// TODO opgave 1
+
+
+	}
+
+	/**
+	 * Returnerer det samlede antal fravaerdage over 12 måneder for eleven med
+	 * elevnr.
+	 *
+	 * @param fravaer
+	 * @param elevNr
+	 * @return
+	 */
+	public int samletFravaer(int[][] fravaer, int elevNr) {
+		// TODO opgave 2
+		return -1;
+	}
+
+	/**
+	 * Returnerer det gennemsnitlige antal fraværsdage pr måned for eleven med
+	 * elevNr.
+	 *
+	 * @param fravaer
+	 * @param elevNr
+	 * @return
+	 */
+	public double gennemsnit(int[][] fravaer, int elevNr) {
+		// TODO opgave 3
+		return -1;
+	}
+
+	/**
+	 * Returnerer antallet af elever der ikke har haft fravær i de 12 måneder.
+	 *
+	 * @param fravaer
+	 * @return
+	 */
+	public int antalUdenFravaer(int[][] fravaer) {
+		// TODO opgave 4
+		return -1;
+	}
+
+	/**
+	 * Returnerer elevNr for den elev der har haft mest fravær igennem de 12
+	 * måneder. Hvis flere elever har haft højst fravær, returneres elevnummer
+	 * for en af disse.
+	 *
+	 * @param fravaer
+	 * @return
+	 */
+	public int mestFravaer(int[][] fravaer) {
+		// TODO opgave 5
+		return -1;
+	}
+
+	/**
+	 * Registrerer at elenven med elevNr ikke har haft fravær i nogen af de 12
+	 * måneder.
+	 *
+	 * @param fravaer
+	 * @param elevNr
+	 */
+	public void nulstil(int[][] fravaer, int elevNr) {
+		// TODO opgave 6
+	}
+}
