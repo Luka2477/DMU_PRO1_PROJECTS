7f3717119b4a689acd4868c70d918e26d57927cd
Luka2477 <luke.da.duke@hotmail.com>
2021-11-12 12:11:41 +0100

    Something

--- Project4_KAS/src/application/model/Hotel.java
+++ Project4_KAS/src/application/model/Hotel.java
@@ -1,4 +1,69 @@
 package application.model;
 
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private String name;
+    private String address;
+
+    private final ArrayList<Conference> conferences = new ArrayList<>();
+    private final ArrayList<HotelRoom> hotelRooms;
+
+    public Hotel (String name, String address, ArrayList<HotelRoom> hotelRooms) {
+        this.name = name;
+        this.address = address;
+
+        this.hotelRooms = hotelRooms;
+    }
+
+    // ----------------------------------------------------------------------------
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public String getAddress () {
+        return this.address;
+    }
+
+    public void setAddress (String address) {
+        this.address = address;
+    }
+
+    // ----------------------------------------------------------------------------
+
+    public void addConference (Conference conference) {
+        if (!this.conferences.contains(conference)) {
+            this.conferences.add(conference);
+        }
+    }
+
+    public void removeConference (Conference conference) {
+        this.conferences.remove(conference);
+    }
+
+    public ArrayList<Conference> getConferences () {
+        return new ArrayList<>(this.conferences);
+    }
+
+    // ----------------------------------------------------------------------------
+
+    public HotelRoom createHotelRoom (int nr, int price, boolean isSingle) {
+        HotelRoom hotelRoom = new HotelRoom(nr, price, isSingle);
+        this.hotelRooms.add(hotelRoom);
+        return hotelRoom;
+    }
+
+    public void removeHotelRoom (HotelRoom hotelRoom) {
+        this.hotelRooms.remove(hotelRoom);
+    }
+
+    public ArrayList<HotelRoom> getHotelRooms () {
+        return new ArrayList<>(this.hotelRooms);
+    }
 }
