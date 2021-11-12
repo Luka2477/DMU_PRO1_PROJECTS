aee4afe79282af589f40cbfcbaf8becf32ac1565
Luka2477 <luke.da.duke@hotmail.com>
2021-11-12 11:49:02 +0100

    Something

--- Project4_KAS/src/application/model/AddOn.java
+++ Project4_KAS/src/application/model/AddOn.java
@@ -1,4 +1,29 @@
 package application.model;
 
 public class AddOn {
+
+    private String name;
+    private int price;
+
+    public AddOn (String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public int getPrice () {
+        return this.price;
+    }
+
+    public void setPrice (int price) {
+        this.price = price;
+    }
+
 }
--- Project4_KAS/src/application/model/HotelRoom.java
+++ Project4_KAS/src/application/model/HotelRoom.java
@@ -1,4 +1,60 @@
 package application.model;
 
+import java.util.ArrayList;
+
 public class HotelRoom {
+
+    private int nr;
+    private int price;
+    private boolean isSingle;
+
+    private ArrayList<AddOn> addOns = new ArrayList<>();
+
+    public HotelRoom (int nr, int price, boolean isSingle) {
+        this.nr = nr;
+        this.price = price;
+        this.isSingle = isSingle;
+    }
+
+    // -------------------------------------------------------------------------------
+
+    public int getNr () {
+        return this.nr;
+    }
+
+    public void setNr (int nr) {
+        this.nr = nr;
+    }
+
+    public int getPrice () {
+        return this.price;
+    }
+
+    public void setPrice (int price) {
+        this.price = price;
+    }
+
+    public boolean isSingle () {
+        return this.isSingle;
+    }
+
+    public void setSingle (boolean single) {
+        this.isSingle = single;
+    }
+
+    // -------------------------------------------------------------------------------
+
+    public void addAddOn (AddOn addOn) {
+        if (!this.addOns.contains(addOn)) {
+            this.addOns.add(addOn);
+        }
+    }
+
+    public void removeAddOn (AddOn addOn) {
+        this.addOns.remove(addOn);
+    }
+
+    public ArrayList<AddOn> getAddOns () {
+        return new ArrayList<>(this.addOns);
+    }
 }
